3e8f0563042a7072bd71c6ab1c2080a3bc2045a8
William Gitau <dontysh@gmail.com>
2024-11-24 00:13:57 +0300

    passes tests

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,15 @@ import requests
 import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        # Send a GET request to the URL and return the response body as bytes
+        response = requests.get(self.url)
+        return response.content  # Use .content to get the raw byte response
 
     def load_json(self):
-        pass
+        # Use get_response_body to get the raw response and parse it as JSON
+        response_body = self.get_response_body()
+        return json.loads(response_body.decode('utf-8'))  # Decode bytes to string and parse as JSON
